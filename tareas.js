17dd33540f5a0e590b1bb6c92c5c449555f69d4a
Jean Aldair <72543108@continental.edu.pe>
2026-05-29 16:12:48 -0500

    Update tareas.js

--- tareas.js
+++ tareas.js
@@ -1,3 +1,3 @@
-console.log("Agregar tarea 1");
-console.log("Agregar tarea 2");
-console.log("Agregar tarea 3");
+console.log("Estudiar tarea 1");
+console.log("Estudiar tarea 2");
+console.log("Estudiar tarea 3");
